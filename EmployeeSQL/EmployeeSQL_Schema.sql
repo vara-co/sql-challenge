0897068a7a46d560d723f92b5bd4cf09c12cd113
Laura "Missha" Vara <152572519+vara-co@users.noreply.github.com>
2024-02-27 15:53:12 -0700

    Update with Value length for columns
    Originally the instructor suggested that to avoid errors the value length for columns wasn't necessary.  However, because it is 10 points of the Data Engineering evaluation of the challenge, I went ahead and added values.

--- EmployeeSQL/EmployeeSQL_Schema.sql
+++ EmployeeSQL/EmployeeSQL_Schema.sql
@@ -2,24 +2,24 @@
 --           EMPLOYEE SQL SCHEMA 
 -----------------------------------------------------
 CREATE TABLE "titles" (
-    "title_id" VARCHAR NOT NULL,
-    "title" VARCHAR NOT NULL,
+    "title_id" VARCHAR(30) NOT NULL,
+    "title" VARCHAR(40) NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY ("title_id")
 );
 
 CREATE TABLE "departments" (
-    "dept_no" VARCHAR NOT NULL,
-    "dept_name" VARCHAR NOT NULL,
+    "dept_no" VARCHAR(20) NOT NULL,
+    "dept_name" VARCHAR(30) NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY ("dept_no")
 );
 
 CREATE TABLE "employees" (
     "emp_no" INT NOT NULL,
-    "emp_title_id" VARCHAR NOT NULL,
+    "emp_title_id" VARCHAR(30) NOT NULL,
     "birth_date" DATE NOT NULL,
-    "first_name" VARCHAR NOT NULL,
-    "last_name" VARCHAR NOT NULL,
-    "sex" VARCHAR NOT NULL,
+    "first_name" VARCHAR(30) NOT NULL,
+    "last_name" VARCHAR(30) NOT NULL,
+    "sex" VARCHAR(10) NOT NULL,
     "hire_date" DATE NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY ("emp_no"),
     CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY ("emp_title_id") REFERENCES "titles" ("title_id")
@@ -34,14 +34,14 @@ CREATE TABLE "salaries" (
 
 CREATE TABLE "dept_emp" (
     "emp_no" INT NOT NULL,
-    "dept_no" VARCHAR NOT NULL,
+    "dept_no" VARCHAR(20) NOT NULL,
     CONSTRAINT "pk_dept_emp" PRIMARY KEY ("emp_no", "dept_no"),
     CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY ("emp_no") REFERENCES "employees" ("emp_no"),
     CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY ("dept_no") REFERENCES "departments" ("dept_no")
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR NOT NULL,
+    "dept_no" VARCHAR(20) NOT NULL,
     "emp_no" INT NOT NULL,
     CONSTRAINT "pk_dept_manager" PRIMARY KEY ("dept_no", "emp_no"),
     CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY ("dept_no") REFERENCES "departments" ("dept_no"),
